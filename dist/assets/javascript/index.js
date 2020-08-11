de110ab23d1adc698c09ab9ceaa60535eb79b450
Mathias Ferniss <5211966@RTS-365.dk>
2020-08-11 09:18:00 +0200

    Er noget til at lave burgermenu

---
+++ dist/assets/javascript/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// let test = fetch("http://localhost:4000/assets/1", {
+//         "method": "GET"
+//     })
+//     .then(data => {
+//         console.log(data)
+//         document.querySelector('.img').src = data.url
+//     })
+//     .catch(err => console.error(err));
+function myFunction() {
+  var x = document.getElementById("myLinks");
+
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
