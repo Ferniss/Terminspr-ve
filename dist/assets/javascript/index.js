56d815aa836ead88c1fe6ab26cf864a8cdaa19ee
Mathias Ferniss <5211966@RTS-365.dk>
2020-08-12 09:11:03 +0200

    hele header virker på mobil og computer, jeg er igang med at strukturerer resten af home siden

--- dist/assets/javascript/index.js
+++ dist/assets/javascript/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
-// let test = fetch("http://localhost:4000/assets/1", {
-//         "method": "GET"
-//     })
-//     .then(data => {
-//         console.log(data)
-//         document.querySelector('.img').src = data.url
-//     })
-//     .catch(err => console.error(err));
+fetch("http://localhost:4000/assets/1", {
+  "method": "GET"
+}).then(function (response) {
+  return response.json();
+}).then(function (data) {
+  console.log(data); // document.querySelector('.src').src = data.url
+});
+
 function myFunction() {
   var x = document.getElementById("myLinks");
 
@@ -17,3 +17,10 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+var section1 = document.querySelector(".home_section-1");
+section1.innerHTML = "\n  <header>\n  <h1>WELCOME IN <br>NIGHT<span class=\"section1_header\">CLUB</span></psan></h1>\n  <img src=\"\" alt=\"\">\n</header>\n<figure>\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n</figure>\n  ";
+var section2 = document.querySelector(".home_section-2");
+section2.innerHTML = "\n  <header>\n  <h1>EVENTS OF THE <br> MOTH</h1>\n  <img src=\"\" alt=\"\">\n</header>\n<figure>\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n</figure>\n  ";
+var section3 = document.querySelector(".home_section-3");
+section3.innerHTML = "\n  <header>\n  <h1>NIGHTCLUB GALLERY</h1>\n  <img src=\"\" alt=\"\">\n</header>\n<figure>\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n  <img src=\"assets/images/Logo.png\" alt=\"\">\n</figure>\n  ";
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,13 @@
-// let test = fetch("http://localhost:4000/assets/1", {
-//         "method": "GET"
-//     })
-//     .then(data => {
-//         console.log(data)
-//         document.querySelector('.img').src = data.url
-//     })
-//     .catch(err => console.error(err));
+    fetch("http://localhost:4000/assets/1", {
+      "method": "GET"
+    })
+    .then(response => response.json())
+.then(data =>{
+  console.log(data)
+  // document.querySelector('.src').src = data.url
+});
+
+
 function myFunction() {
     let x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -14,3 +16,43 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+  let section1 = document.querySelector(".home_section-1")
+  section1.innerHTML = `
+  <header>
+  <h1>WELCOME IN <br>NIGHT<span class="section1_header">CLUB</span></psan></h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+</figure>
+  `
+
+  let section2 = document.querySelector(".home_section-2")
+  section2.innerHTML = `
+  <header>
+  <h1>EVENTS OF THE <br> MOTH</h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="assets/images/Logo.png" alt="">
+</figure>
+  `
+
+  let section3 = document.querySelector(".home_section-3")
+  section3.innerHTML = `
+  <header>
+  <h1>NIGHTCLUB GALLERY</h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+  <img src="assets/images/Logo.png" alt="">
+</figure>
+  `
