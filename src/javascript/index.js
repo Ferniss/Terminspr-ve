9d1efa7ca031e6375db6500fbec4dcfc7437593d
Mathias Ferniss <5211966@RTS-365.dk>
2020-08-12 11:16:44 +0200

    Er igang med computer siden til mainhome

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -21,7 +21,7 @@
   <h1>WELCOME IN <br>NIGHT<span class="section1_header">CLUB</span></psan></h1>
   <img src="" alt="">
 </header>
-<figure>
+<figure class="home_figure-1">
   <img src="assets/images/content-img/thumb1.jpg" alt="" class"main_img">
   <img src="assets/images/content-img/reastaurant_1.jpg" alt="" class"main_img">
   <img src="assets/images/content-img/thumb2.jpg" alt="" class"main_img">
@@ -46,8 +46,8 @@
   <h1>NIGHTCLUB GALLERY</h1>
   <img src="" alt="">
 </header>
-<figure>
-<img src="${data[15].url}" alt="">
+<figure class="home_figure-3">
+  <img src="${data[15].url}" alt="">
   <img src="${data[16].url}" alt="">
   <img src="${data[17].url}" alt="">
   <img src="${data[18].url}" alt="">
@@ -57,4 +57,64 @@
 </figure>
   `
 
+  let section4 = document.querySelector(".home_section-4")
+  section4.innerHTML = `
+  <header>
+  <h1>NIGHT CLUB TRACK</h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="${data[22].url}" alt="">
+
+</figure>
+  `
+  let section5 = document.querySelector(".home_section-5")
+  section5.innerHTML = `
+  <header>
+  <h1>LATEST VIDEO</h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="${data[22].url}" alt="">
+
+</figure>
+  `
+
+  let section6 = document.querySelector(".home_section-6")
+  section6.innerHTML = `
+  <header>
+  <h1>RECENT BLOG</h1>
+  <img src="" alt="">
+</header>
+<figure>
+  <img src="${data[1].url}" alt="">
+<figcaption>
+<h4>MORE THAN 20 YEA...</h4>
+<h6>BY: Admin/3 Comments/16 Nov 2018</h6>
+<p>
+Its a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+</p>
+</figcaption>
+</figure>
+<figure>
+  <img src="${data[2].url}" alt="">
+<figcaption>
+<h4>MORE THAN 20 YEA...</h4>
+<h6>BY: Admin/3 Comments/16 Nov 2018</h6>
+<p>
+Its a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+</p>
+</figcaption>
+</figure>
+<figure>
+  <img src="${data[0].url}" alt="">
+<figcaption>
+<h4>MORE THAN 20 YEA...</h4>
+<h6>BY: Admin/3 Comments/16 Nov 2018</h6>
+<p>
+Its a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+</p>
+</figcaption>
+</figure>
+  `
 });
